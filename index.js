d4bb01502c3b1cb6e99ef2dd122763322fccc82f
Mitya Kim <mitya_kim@icloud.com>
2021-12-16 10:41:26 -0800

    third commit

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const path = require('path')
 
 const app = express()
 
-app.get('/', (req,res) => {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 const port = process.env.PORT || 4545
 
 app.listen(4545, () => console.log(`Go to port ${port}`))
+
