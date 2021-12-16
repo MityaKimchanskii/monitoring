cc1def51947434f68180d83852d58e8fad5691a9
Mitya Kim <mitya_kim@icloud.com>
2021-12-16 11:55:13 -0800

    commit 1

--- server/index.js
+++ server/index.js
@@ -3,9 +3,17 @@ const path = require('path')
 
 const app = express()
 
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: 'ab7b270ef9e24c61874ff1ff409aee8a'
+})
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+
 const port = process.env.PORT || 4005
 app.listen(port, () => {console.log(`port ${port}.`)})
+
