8d381cb98c4ef517af68fe93bdd0e5d40a07a8e9
Mitya Kim <mitya_kim@icloud.com>
2021-12-16 12:14:20 -0800

    message rollbar

--- server/index.js
+++ server/index.js
@@ -10,11 +10,39 @@ let rollbar = new Rollbar({
 
 const app = express()
 
+app.use(express.json())
+app.use('/style', express.static('./public/styles.css'))
+
+let students = []
+
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully.')
 })
 
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
+})
+
 const port = process.env.PORT || 4545
+
+// rolly
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => console.log(`Take us to warp ${port}!`))
