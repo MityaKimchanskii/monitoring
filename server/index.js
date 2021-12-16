c9056e8e88b011a832e2e12cdc8412cec1fff2e6
Mitya Kim <mitya_kim@icloud.com>
2021-12-16 11:10:46 -0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = process.env.PORT || 4005
+app.listen(port, () => {console.log(`port ${port}.`)})
